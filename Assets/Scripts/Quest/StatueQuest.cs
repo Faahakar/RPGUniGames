c9a40ab2e07c69c369c1f3b7a742c92d33201bc5
ATobolsky <51061937+ATobolsky@users.noreply.github.com>
2020-05-09 11:49:52 -0300

    Gauchera
    commit para alinhar

---
+++ Assets/Scripts/Quest/StatueQuest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.StatueQuest
+{
+
+    public class StatueQuest : MonoBehaviour{
+
+        public GameObject HUD_StatueQuest;
+        public GameObject HUD_PressF;
+        bool isPegandoQuest;
+
+        void Start(){
+            HUD_StatueQuest.SetActive (false);
+            HUD_PressF.SetActive (false);            
+        }
+
+        void Update(){
+            if(Input.GetKeyDown(KeyCode.F) && isPegandoQuest) {
+                HUD_StatueQuest.SetActive (true);
+                HUD_PressF.SetActive (false);       
+            }
+        }
+
+        void OnTriggerEnter(Collider hit){
+            if(hit.gameObject.tag == "Player"){
+                //HUD_StatueQuest.SetActive (true);
+                isPegandoQuest = true;
+                HUD_PressF.SetActive (true);
+            }
+        }
+
+        void OnTriggerExit(Collider hit){
+            if(hit.gameObject.tag == "Player"){
+                HUD_StatueQuest.SetActive (false);
+                HUD_PressF.SetActive (false);
+                isPegandoQuest = false;
+            }
+        }
+    }
+}
